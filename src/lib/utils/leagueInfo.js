44f9ee2f58f7ee92426e0055b35cafc7be57ebf6
tathya9223 <74516064+tathya9223@users.noreply.github.com>
2024-05-30 21:33:31 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -28,7 +28,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Atlanta", // (optional)
       "bio": "Would 1v1 the whole city of Dallas",
-      "photo": "/managers/tat.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "src/lib/Managers/tat.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2013, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
